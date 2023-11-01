2b106e199f92cc87727446d68eb2e8a5633317d5
urumil <144309347+urumil@users.noreply.github.com>
2023-11-01 13:38:32 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang ="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>連想配列を作って値とキーを出力しよう</title>
+ </head>
+
+ <body>
+   <p>
+     <?php
+     $food_data = ['name' => 'onion','price' => 200,'weight' => 160];
+     print_r($food_data);
+     ?>
+    </p>
+ </body>
+</html>
